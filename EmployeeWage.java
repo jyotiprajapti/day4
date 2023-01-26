e4c6bcca249e8c8251b2c3bf6ad33b3a29aac739
jyotiprajapti <jyotiprajapati3029@gmail.com>
2023-01-26 20:30:29 +0530

    UC1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,17 @@
 package com.day4;
-
+import java.lang.Math;
 public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-
+         double x = Math.random();
+         if(x>=0.5) {
+        	 System.out.println("employee is present");
+        	 
+         }
+         else {
+        	 System.out.println("employee is absent");
+         }
+         
 		}
 	}
 	
